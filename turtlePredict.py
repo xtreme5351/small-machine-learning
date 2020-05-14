15be94b15a583a8a504290202e7fc4524bf27d0d
xtreme5351 <45394613+xtreme5351@users.noreply.github.com>
2020-05-14 22:37:15 +0100

    Added outcome errors and some cleanup

--- turtlePredict.py
+++ turtlePredict.py
@@ -4,60 +4,72 @@ import turtle
 import random
 totalAng = []
 totalLst = []
+totalErrorA = []
+totalErrorD = []
 ang = []
 lst = []
 n = 0
+turtle.speed(100)
 
-# total angle 
-
-
+# total angle
 def tang(ang):
     return sum(ang)
 
-# total distance travelled
-
-
+# total sum of list
 def tlst(lst):
     return sum(lst)
 
-# average distance travelled
 
 def avg(lst):
     return sum(lst) / len(lst)
 
-# average angle turned
 
 def angle(ang):
     return sum(ang) / len(ang)
 
+
+# predicted distance of movement (self-made algorithim)
+def prdy(totalL, totalA, average, avgDist):
+    return (totalA / totalL - 3.1) + ((1 + avgDist / average) * avgDist) * (1 - avgDist / average)
+
+
+# predicted angle of movement (self-made algorithim)
+def prdx(totalL, totalA, average, avgDist):
+    return (190 - (10 * (totalA / totalL))) + ((1 + avgDist / average) * avgDist) * (1 - avgDist / average)
+
+
+
 # n = number of times the code will run, the more it runs, the more accurate the result
-
-
-while n < 50:
+while n < 1000:
     a = random.randint(1, 360)
     d = random.randint(1, 75)
     turtle.right(a)
     turtle.fd(d)
+
     totalAng.append(a)
     ang.append(a)
     lst.append(d)
     avgDist = avg(lst)
     average = avg(ang)
+    print("======")
     print("Average dist: " + str(round(avgDist, 4)) + " Average angle: " + str(round(average, 4)))
+
+    totalL = tlst(lst)
+    totalA = tang(ang)
+    initalPy = prdy(totalL, totalA, average, avgDist)
+    initialPx = prdx(totalL, totalA, average, avgDist)
+    print("Predicted Angle: " + str(round(initialPx, 4)))
+    print("Predicted Dist: " + str(round(initalPy, 4)))
+
+    errorA = (a - initialPx) / a
+    errorD = (d - initalPy) / d
+    totalErrorA.append(errorA)
+    totalErrorD.append(errorD)
+    print("Angular error: ", round(errorA, 4))
+    print("Distance error: ", round(errorD, 4))
     totalLst.append(d)
+
     n += 1
-
-# predicted distance of movement
-
-
-def prdy(totalL, totalA, average, avgDist):
-    return (totalA / totalL - 3.1) + ((1 + avgDist / average) * avgDist) * (1 - avgDist / average)
-
-
-# predicted angle of movement
-
-def prdx(totalL, totalA, average, avgDist):
-    return (190 - (10 * (totalA / totalL))) + ((1 + avgDist / average) * avgDist) * (1 - avgDist / average)
 
 
 average = angle(ang)
@@ -69,8 +81,11 @@ px = prdx(totalL, totalA, average, avgDist)
 
 
 print("Total Dist: " + str(round(totalL, 4)) + " Total Angle: " + str(round(totalA, 4)))
-print("Next predicted Angle of movement: " + str(round(px, 4)))
-print("next predicted Distance of movement: " + str(round(py, 4)))
+print("Final predicted Angle of movement: " + str(round(px, 4)))
+print("Final predicted Distance of movement: " + str(round(py, 4)))
+print("Avg angle error: ", round(avg(totalErrorA), 4))
+print("Avg dist error: ", round(avg(totalErrorD), 4))
+
 
 
 x = ang
